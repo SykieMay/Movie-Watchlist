884e05a207650c5c95b8641891534a3887cd2fa4
SykieMay <smdaroy2024@hccci.edu.ph>
2026-08-22 18:47:03 +0800

    Task 1: move movies into useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import Layout from "./layouts/Layout";
 import MovieList from "./components/MovieList";
-import movies from "./data/movies";
+import moviesData from "./data/movies";
+import { useState } from "react";
 
 export default function App() {
+  const [movies, setMovies] = useState(moviesData);
   return (
     <Layout>
       <div className="mb-6">
